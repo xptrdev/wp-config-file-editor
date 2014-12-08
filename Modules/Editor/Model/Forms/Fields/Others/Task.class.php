c57b29ae7b1a55c6722f18d62909fcba761ef954
AHMeD <xpointer@hotmail.com>
2014-12-08 10:55:28 +0200

    Missing file from previous ocmmits
    Add notes to helpbox + Adding missing file from previous commits!

---
+++ Modules/Editor/Model/Forms/Fields/Others/Task.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* 
+*/
+
+# Define namespace
+namespace WCFE\Modules\Editor\Model\Forms\Fields\Others;
+
+# Field base
+use WPPFW\Forms;
+
+/**
+* 
+*/
+class Task extends Forms\Fields\FormStringField {
+	
+	/**
+	* put your comment there...
+	* 
+	*/
+	public function __construct() {
+		# Set field name and rules
+		parent::__construct('Task', array(new Forms\Rules\RequiredField()));
+	}
+
+}
