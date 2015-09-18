b2598c9263298527e8d2ca2750ccc1dfbbb87afc
AHMeD SAiD <xpointer@hotmail.com>
2015-09-18 11:37:58 +0200

    Dashboard menu help

--- Modules/Editor/View/Editor/Templates/Index.html.php
+++ Modules/Editor/View/Editor/Templates/Index.html.php
@@ -34,19 +34,19 @@ $router =& $this->router();
 		<li>
 			Profiles
 			<ul id="wcfe-dmm-profiles">
-				<li id="wcfe-dmm-profiles-create">Create</li>
-				<li id="wcfe-dmm-profiles-list">Profiles</li>
+				<li id="wcfe-dmm-profiles-create" title="Create new profile from config form fields">Create</li>
+				<li id="wcfe-dmm-profiles-list" title="Manage profiles">Profiles</li>
 				<li>-</li>
 				<li id="wcfe-dmm-profiles-active-profile">
 					Active Profile
 					<ul>
-						<li id="wcfe-dmm-profiles-save">Save</li>
+						<li id="wcfe-dmm-profiles-save" title="Save config form fields vars into active profile">Save</li>
 						<li>-</li>
-						<li id="wcfe-dmm-profiles-edit">Edit</li>
-						<li id="wcfe-dmm-profiles-delete">Delete</li>
-						<li id="wcfe-dmm-profiles-reload">Reload</li>
+						<li id="wcfe-dmm-profiles-edit" title="Edit active profile description">Edit</li>
+						<li id="wcfe-dmm-profiles-delete" title="Delete active profile, return back to config file values, discard changes">Delete</li>
+						<li id="wcfe-dmm-profiles-reload" title="Reload active profile field values, discard changes">Reload</li>
 						<li>-</li>
-						<li id="wcfe-dmm-profiles-unload">Unload</li>
+						<li id="wcfe-dmm-profiles-unload" title="Active profile deattach. Return to config file values">Unload</li>
 					</ul>
 				</li>
 			</ul>
@@ -54,8 +54,8 @@ $router =& $this->router();
 		<li>
 			Tab
 			<ul id="wcfe-dmm-tab">
-				<li id="wcfe-dmm-tab-help">Fields Help</li>
-				<li id="wcfe-dmm-tab-constants-list">Constants List</li>
+				<li id="wcfe-dmm-tab-help" title="Active tab fields help text">Fields Help</li>
+				<li id="wcfe-dmm-tab-constants-list" title="Active tab fields mapped to wp-config file constants list">Constants List</li>
 			</ul>
 		</li>
 
