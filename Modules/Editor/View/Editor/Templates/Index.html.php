1e1dac07b5dd4088a4f2a2ac7dfff3ac78ad0b55
AHMeD SAiD <xpointer@hotmail.com>
2015-09-20 12:34:12 +0200

    Add Profiles and Info Under Tools Menu item

--- Modules/Editor/View/Editor/Templates/Index.html.php
+++ Modules/Editor/View/Editor/Templates/Index.html.php
@@ -32,31 +32,39 @@ $router =& $this->router();
 	<ul id="wcfe-config-form-main-menu">
 		
 		<li>
-			Profiles
-			<ul id="wcfe-dmm-profiles">
-				<li id="wcfe-dmm-profiles-create" title="Create new profile from config form fields">Create</li>
-				<li id="wcfe-dmm-profiles-list" title="Manage profiles">Profiles</li>
-				<li>-</li>
-				<li id="wcfe-dmm-profiles-active-profile">
-					Active Profile
-					<ul>
-						<li id="wcfe-dmm-profiles-save" title="Save config form fields vars into active profile">Save</li>
+			Tools
+			<ul>
+			
+				<li>
+					Profiles
+					<ul id="wcfe-dmm-profiles">
+						<li id="wcfe-dmm-profiles-create" title="Create new profile from config form fields">Create</li>
+						<li id="wcfe-dmm-profiles-list" title="Manage profiles">Profiles</li>
 						<li>-</li>
-						<li id="wcfe-dmm-profiles-edit" title="Edit active profile description">Edit</li>
-						<li id="wcfe-dmm-profiles-delete" title="Delete active profile, return back to config file values, discard changes">Delete</li>
-						<li id="wcfe-dmm-profiles-reload" title="Reload active profile field values, discard changes">Reload</li>
-						<li>-</li>
-						<li id="wcfe-dmm-profiles-unload" title="Active profile deattach. Return to config file values">Unload</li>
+						<li id="wcfe-dmm-profiles-active-profile">
+							Active Profile
+							<ul>
+								<li id="wcfe-dmm-profiles-save" title="Save config form fields vars into active profile">Save</li>
+								<li>-</li>
+								<li id="wcfe-dmm-profiles-edit" title="Edit active profile description">Edit</li>
+								<li id="wcfe-dmm-profiles-delete" title="Delete active profile, return back to config file values, discard changes">Delete</li>
+								<li id="wcfe-dmm-profiles-reload" title="Reload active profile field values, discard changes">Reload</li>
+								<li>-</li>
+								<li id="wcfe-dmm-profiles-unload" title="Active profile deattach. Return to config file values">Unload</li>
+							</ul>
+						</li>
 					</ul>
 				</li>
-			</ul>
-		</li>
+				
+				<li>
+					Info
+					<ul id="wcfe-dmm-info">
+						<li id="wcfe-dmm-info-paths" title="Discover your installation path information">Paths</li>
+					</ul>
+				</li>
 		
-		<li>
-			Info
-			<ul id="wcfe-dmm-info">
-				<li id="wcfe-dmm-info-paths" title="Discover your installation path information">Paths</li>
 			</ul>
+		
 		</li>
 		
 		<li>
